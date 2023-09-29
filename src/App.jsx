ccbef31677d309caf7797da5bca1933e300ff719
Tanmay <cooltanmayvig@gmail.com>
2023-09-30 00:39:58 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+    {/* Navbar Component */}
+    <Navbar />
+      <h1>Hello World</h1>
+
+      {/* Footer component */}
+      <Footer />
+    </>
+  )
+}
+
+export default App
