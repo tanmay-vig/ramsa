789007bfb927686c1e5a444756ea8a9a028fd2c3
Tanmay <cooltanmayvig@gmail.com>
2023-10-04 02:05:20 +0530

    hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,41 @@
-
+import pic1 from "../img/jiit_logo.png";
+import "./css/home.css";
 export default function Home() {
-  return (
-    <>
-      {/* Aryesh yaha par code karna hai */}
-    </>
-  )
+    return (
+        <>
+            {/* Hero Section */}
+            <div className="texture-background">
+                <br />
+                <div className="d-flex aligner ">
+                    {/* For logo */}
+                    <div className="  b-30"> 
+                        <img src={pic1} alt="jiit" className="jiit-logo" />
+                    </div>
+                    {/* For Info */}
+                    <div className=" mx-3 b-70">
+                        <h3>
+                            {" "}
+                            <b>7th International Conference</b> on
+                        </h3>{" "}
+                        <h1 className="text-danger">
+                            Recent Advances in Mathematical Sciences and its
+                            Applications <br />
+                            (RAMSA - 2024){" "}
+                        </h1>
+                        <h3>
+                            {" "}
+                            <b>
+                                Department of Mathematics, <br />
+                                Jaypee Institute of Information Technology{" "}
+                                <br />
+                                (JIIT Noida, U.P.)
+                            </b>
+                        </h3>
+                        <h4 className="text-danger">February 29 - March 02, 2024 (Hybrid Mode)</h4>
+                    </div>
+                </div>
+            </div>
+            {/* Slider section */}
+        </>
+    );
 }
