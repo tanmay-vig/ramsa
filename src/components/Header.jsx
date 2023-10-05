4df0e2930e2a9509ecb4360768c335c2d2046b25
Tanmay <cooltanmayvig@gmail.com>
2023-10-05 11:22:43 +0530

    new changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,75 +1,73 @@
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom'
-import './Header.css'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "./Header.css";
 export default function Header() {
-  return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary" sticky='top'>
-      <Container>
-        <Navbar.Brand href="/">
-          <b>RAMSA</b></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link >
-            <Link to='/' className='l'>
-              Home
-            </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to='/about' className='l'>
-
-              About us
-              </Link>
-              </Nav.Link>
-            <Nav.Link >
-              <Link to="/organizers" className='l'>Organizers</Link>
-              </Nav.Link>
-            <Nav.Link >
-              <Link to="/resourceperson" className='l'>
-
-              Resource Person
-              </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/sessions" className='l'>
-              Sessions
-              </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/papers" className='l'>Papers</Link>
-              
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/registration" className='l'>
-              Registration
-              </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/annoucements" className='l'>
-
-              Annoucements
-              </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/importantdates" className='l'>
-
-              Important Dates
-              </Link>
-              </Nav.Link>
-            <Nav.Link>
-              <Link to="/contact" className='l'>
-
-              Contact Us
-              </Link>
-              </Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </>
-  )
+    return (
+        <>
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                className=""
+                sticky="top"
+            >
+                <Container>
+                    <Navbar.Brand href="/" className="text-white">
+                        <b>RAMSA</b>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link eventKey="1">
+                                <Link to="/about" className="l">
+                                    About us
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="2">
+                                <Link to="/organizers" className="l">
+                                    Organizers
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="3">
+                                <Link to="/resourceperson" className="l">
+                                    Resource Person
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="4">
+                                <Link to="/sessions" className="l">
+                                    Sessions
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="5">
+                                <Link to="/papers" className="l">
+                                    Papers
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="6">
+                                <Link to="/registration" className="l">
+                                    Registration
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="7">
+                                <Link to="/annoucements" className="l">
+                                    Annoucements
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="8">
+                                <Link to="/importantdates" className="l">
+                                    Important Dates
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link eventKey="9">
+                                <Link to="/contact" className="l">
+                                    Contact Us
+                                </Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
