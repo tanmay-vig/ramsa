4a7db83523d36ab9eb5224d4898c27d591a54712
Tanmay <cooltanmayvig@gmail.com>
2023-10-27 00:46:47 +0530

    previous

--- src/pages/Previous.jsx
+++ src/pages/Previous.jsx
@@ -22,42 +22,185 @@ import pic12 from "../img/prevConf/2022-1.jpeg";
 export default function Previous() {
     return (
         <>
-            <div>
-                <div className="">
-                    <img src={pic1} alt="2016-1" />
-                    <img src={pic2} alt="2016-2" />
-                </div>
-            </div>
-            <div>
+            <section>
                 <div>
-                    <img src={pic3} alt="2017-1" />
-                    <img src={pic4} alt="2017-2" />
-                </div>
-            </div>
-            <div>
+                    <div className="red-head center">RAMSA 2016</div>
+                    <div className="center text-section">
+                        First International Conference on Recent Advances in
+                        Mathematical Sciences and its Applications (RAMSA-2016)
+                        was organized on December 08-10, 2016 at Jaypee
+                        Institute of Information Technology, Noida. The
+                        objective of this conference was to bring together
+                        learned mathematicians, scientists, engineers,
+                        researchers and to provide a platform for exchange of
+                        ideas regarding the recent research findings and new
+                        advances. The oral presentations were held in six
+                        different tracks across diverse set of themes and there
+                        were 13 Plenary/Keynote speakers, all across the world
+                        from various reputed Institutions/Universities. The
+                        conference proceedings of RAMSA-2016 were published by
+                        AIP (Scopus Indexed), USA.
+                    </div>{" "}
+                    <br />
+                    <div className="text-center">
+
+                    <a href="https://aip.scitation.org/toc/apc/1802/1"><button className="btn btn-success">View
+                                Proceedings Here</button></a>
+                    </div> <br />
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic1} alt="2016-1" />
+                        <img src={pic2} alt="2016-2" />
+                    </div>
+                </div>{" "}
+                <br />
                 <div>
-                    <img src={pic5} alt="2019-1" />
-                    <img src={pic6} alt="2019-2" />
-                </div>
-            </div>
-            <div>
+                    <div className="red-head center">RAMSA 2017</div>
+                    <div className="text-section center">
+                        2<sup>nd</sup> International Conference on Recent
+                        Advances in Mathematical Sciences and its Applications
+                        (RAMSA-2017) was organized on December 12-14, 2017 at
+                        Jaypee Institute of Information Technology, Noida. The
+                        theme of this conference successfully addressed all
+                        these challenging aspects of advancement of mathematics
+                        including almost all active research areas in pure,
+                        applied and inter- disciplinary mathematics and their
+                        applications. The oral presentations were held in six
+                        different tracks across diverse set of themes and there
+                        were 13 Plenary/Keynote speakers, all across the world
+                        from various reputed Institutions/Universities. The
+                        conference proceedings of RAMSA-2017 were published by
+                        AIP (Scopus Indexed), USA.
+                    </div> <br />
+                    <div className="text-center">
+                        
+                    <a href="https://aip.scitation.org/toc/apc/1897/1" ><button className="btn btn-success">View
+                                Proceedings Here</button></a>
+                    </div> <br />
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic3} alt="2017-1" />
+                        <img src={pic4} alt="2017-2" />
+                    </div>
+                </div>{" "}
+                <br />
                 <div>
-                    <img src={pic7} alt="2020-1" />
-                    <img src={pic8} alt="2020-2" />
-                </div>
-            </div>
-            <div>
+                    <div className="red-head center">RAMSA 2019</div>
+                    <div className="text-section center">
+                        Third International Conference on Recent Advances in
+                        Mathematical Sciences and its Applications (RAMSA-2019)
+                        was organized on Jan 17-19, 2019 at Jaypee Institute of
+                        Information Technology, Noida. Almost all active
+                        research areas in Pure, Applied and Inter-disciplinary
+                        Mathematics across the globe have been included in the
+                        theme of RAMSA-2019. Thus, the progress in research
+                        areas related to Mathematics, Statistics, Electronics
+                        and Applied Physics, Computer Sciences and Informatics
+                        and Earth Sciences etc are reflected. The oral
+                        presentations were held in five different tracks across
+                        diverse set of themes and there were 12 Plenary/Keynote
+                        speakers, all across the world from various reputed
+                        Institutions/Universities. The conference proceedings of
+                        RAMSA-2019 were published by AIP (Scopus Indexed), USA.
+                    </div> <br />
+                    <div className="text-center">
+                        
+                    <a href="https://aip.scitation.org/toc/apc/2061/1" ><button className="btn btn-success">View
+                                Proceedings Here</button></a>
+                    </div> <br />
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic5} alt="2019-1" />
+                        <img src={pic6} alt="2019-2" />
+                    </div>
+                </div> <br />
                 <div>
-                    <img src={pic9} alt="2021-1" />
-                    <img src={pic10} alt="2021-2" />
-                </div>
-            </div>
-            <div>
+                    <div className="red-head center">RAMSA 2020</div>
+                    <div className="text-section center">
+                        4<sup>th</sup> International conference on Recent
+                        Advances in Mathematical Sciences and its Applications
+                        (RAMSA-2020) was organized on Jan 09-11, 2020 at Jaypee
+                        Institute of Information Technology, Noida. The
+                        objective of this conference was to bring together
+                        learned mathematicians, scientists, engineers,
+                        researchers and to provide a platform for exchange of
+                        ideas regarding the recent research findings and new
+                        advances. Further, it will be able to identify the
+                        frontline research areas of vital importance from
+                        applications point of view. The oral presentations were
+                        held in five different tracks across diverse set of
+                        themes and there were 11 Plenary/Keynote speakers, all
+                        across the world from various reputed
+                        Institutions/Universities. The conference proceedings of
+                        RAMSA-2020 were published by AIP (Scopus Indexed), USA.
+                    </div> <br />
+                    <div className="text-center">
+                        
+                    <a href="https://aip.scitation.org/toc/apc/2214/1" ><button className="btn btn-success">View
+                                Proceedings Here</button></a>
+                    </div>
+                    <br />
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic7} alt="2020-1" />
+                        <img src={pic8} alt="2020-2" />
+                    </div>
+                </div> <br />
                 <div>
-                    <img src={pic11} alt="2022-1" />
-                    <img src={pic12} alt="2022-2" />
+                    <div className="red-head center">RAMSA 2021</div>
+                    <div className="text-section center">
+                        5<sup>th</sup> International conference on Recent
+                        Advances in Mathematical Sciences and its Applications
+                        (RAMSA-2020) was organized on December 02-04, 2021 at
+                        Jaypee Institute of Information Technology, Noida. The
+                        objective of this conference was to facilitate exchange
+                        of ideas regarding the research findings and new
+                        advances in mathematics in the wide area of mathematics,
+                        sciences and engineering and to discuss the practical
+                        challenges encountered in the different domains of
+                        applications and the solutions adopted thereto. The oral
+                        presentations were held in five different tracks across
+                        diverse set of themes and there were 13 Plenary/Keynote
+                        speakers, all across the world from various reputed
+                        Institutions/Universities. The conference proceedings of
+                        RAMSA-2021 were published by NOVA Publishers, USA.
+                    </div> 
+                    <br />
+                    <div className="text-center">
+                        
+                    <a href="https://novapublishers.com/shop/emerging-advancements-in-mathematical-sciences/" ><button
+                                className="btn btn-success">View
+                                Proceedings Here</button></a>
+                    </div>
+                    <br />
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic9} alt="2021-1" />
+                        <img src={pic10} alt="2021-2" />
+                    </div>
+                </div> <br />
+                <div>
+                    <div className="red-head center">RAMSA 2022</div>
+                    <div className="text-section center">
+                        The 6<sup>th</sup> International conference on Recent
+                        Advances in Mathematical Sciences and its Applications
+                        (RAMSA-2022) took place from December 08 to December 10,
+                        2022, at the Jaypee Institute of Information Technology,
+                        Noida. Notably, RAMSA-2022 was organized as a hybrid
+                        conference, blending the virtual and in-person
+                        experiences. The primary objective of this conference
+                        was to facilitate the exchange of ideas regarding
+                        research findings and new advances in the wide area of
+                        mathematics, sciences, and engineering.The oral
+                        presentations were categorized into five different
+                        tracks, spanning a diverse set of themes, and featured
+                        12 Plenary/Keynote speakers from various reputed
+                        Institutions/Universities worldwide. RAMSA-2022 served
+                        as a bridge between the global mathematical community
+                        and the advancement of knowledge in this
+                        multidisciplinary field.
+                    </div>
+                    <div className="d-flex flex-wrap justify-content-center">
+                        <img src={pic11} alt="2022-1" />
+                        <img src={pic12} alt="2022-2" />
+                    </div>
                 </div>
-            </div>
+            </section>
         </>
     );
 }
