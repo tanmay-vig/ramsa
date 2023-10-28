a342584fad1d4999002c1cadef3310c9ede7615a
Tanmay <cooltanmayvig@gmail.com>
2023-10-28 14:14:05 +0530

    gg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,72 +2,110 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Header.css";
 export default function Header() {
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" sticky="top">
+            <Navbar expanded={expanded} expand="lg" sticky="top">
                 <Container>
                     <Navbar.Brand href="/" className="text-white">
                         <b>RAMSA</b>
                     </Navbar.Brand>
                     <Navbar.Toggle
+                        onClick={() =>
+                            setExpanded(expanded ? false : "expanded")
+                        }
                         aria-controls="basic-navbar-nav"
                         className="bg-white"
                     />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="">
-                            <div className="mx-2">
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/about" className="l">
                                     About us
                                 </Link>
-                            </div>
-                            <div className="mx-2">
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/organizers" className="l">
                                     Organizers
                                 </Link>
-                            </div>
+                            </Nav.Item>
 
-                            <div className="mx-2">
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <a
                                     href="https://sites.google.com/view/ramsa-2024/speakers"
                                     className="l"
                                 >
                                     Speakers
                                 </a>
-                            </div>
-                            <div className="mx-2">
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/sessions" className="l">
                                     Call For Sessions
                                 </Link>
-                            </div>
-                            <div className="mx-2">
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/papers" className="l">
                                     Call For Papers
                                 </Link>
-                            </div>
-                            
-                            <div className="mx-2">
+                            </Nav.Item>
+
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/registration" className="l">
                                     Registration
                                 </Link>
-                            </div>
-                            <div className="mx-2">
-                                <a href="https://sites.google.com/view/ramsa-24-important-dates/home" className="l">
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
+                                <a
+                                    href="https://sites.google.com/view/ramsa-24-important-dates/home"
+                                    className="l"
+                                >
                                     Important Dates
                                 </a>
-                            </div>
-                            <div className="mx-2">
-                                <a href="https://sites.google.com/view/ramsa-24-updates/home" className="l" >
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
+                                <a
+                                    href="https://sites.google.com/view/ramsa-24-updates/home"
+                                    className="l"
+                                >
                                     Updates
                                 </a>
-                            </div>
-                            <div className="mx-2">
+                            </Nav.Item>
+                            <Nav.Item
+                                className="mx-2"
+                                onClick={() => setExpanded(false)}
+                            >
                                 <Link to="/contact" className="l">
                                     Contact Us
                                 </Link>
-                            </div>
-                            
+                            </Nav.Item>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
