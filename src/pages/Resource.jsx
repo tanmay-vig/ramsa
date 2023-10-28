88bfd6a53ea85d01b16d102cc550204056f9ea7c
Tanmay <cooltanmayvig@gmail.com>
2023-10-28 10:42:52 +0530

    d

--- src/pages/Resource.jsx
+++ src/pages/Resource.jsx
@@ -2,7 +2,7 @@
 export default function Resource() {
   return (
     <>
-    <h2 className="text-center my-3">Resource Person</h2>
+    <h2 className="text-center my-3">Speakers</h2>
     <div className="text-center">
        To be uploaded....
     </div>
