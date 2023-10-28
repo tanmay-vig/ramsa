17a9172a0136c8c4156d5616a2d6f3b662c40731
Tanmay <cooltanmayvig@gmail.com>
2023-10-28 10:39:35 +0530

    dd

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
                     </div>
                     <hr />
                      <br />
-                    <div className="text-left">
+                    <div className="d">
                         The Jaypee Institute of Information Technology (JIIT), a
                         deemed-to-be University under the UGC Act 1956, stands
                         as the gracious host for the 7th International
@@ -28,7 +28,7 @@ export default function About() {
                         programs in Engineering, Sciences, and Management.
                     </div>
                      <br />
-                    <div className="text-left">
+                    <div className="d">
                         Notably, the Department of Mathematics, an integral part
                         of JIIT, plays a vital role in advancing scientific
                         research, especially in mathematics and its
