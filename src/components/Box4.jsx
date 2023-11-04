5ffc85c4ac19c1a82455a3a1d748735f0621332f
Aryesh Srivastava <aryeshsrivastava@gmail.com>
2023-11-04 11:57:17 +0530

    commit-org

--- src/components/Box4.jsx
+++ src/components/Box4.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box4a from './Box4a';
 import Box4b from './Box4b';
 import '../pages/Box2.css'
+import '../pages/App.css'
 
 function Box4() {
     // const border = {
@@ -21,10 +22,21 @@ function Box4() {
   return (
     <div>
         <Box4a name = "DEPARTMENTAL COMMITTEE"></Box4a>
-        <div className='style4'>
-            <div style={{marginRight:"70px"}}>
-                <Box4b name1= "Prof. Bhagwati Prasad Chamola," position1="Professor"></Box4b>
+        <div className='style2'>
+            <div className='style'>
+                <Box4b name1= "Prof. B. P. Chamola," position1="Professor"></Box4b>
+                <div className='style-special4'></div>
+                <div className='style-special5'></div>
+                <Box4b name1= "Prof. Lokendra Kumar," position1="Professor"></Box4b>
+            </div>
+            <div className='style'>
                 <Box4b name1= "Prof. Amit Srivastava," position1="Professor"></Box4b>
+                <div className='style-special3'></div>
+                <Box4b name1= "Dr.  Lakhveer Kaur," position1="Associate Professor"></Box4b>
+            </div>
+        </div>
+        <div className='style4'>
+            <div className='box-4style'>
                 <Box4b name1= "Dr. Kamlesh Kumar Shukla," position1="Assistant Professor"></Box4b>
                 <Box4b name1= "Dr.  Neha Singhal," position1="Assistant Professor"></Box4b>
                 <Box4b name1= "Dr. Manish Kumar Bansal," position1="Assistant Professor"></Box4b>
@@ -34,9 +46,7 @@ function Box4() {
                 <Box4b name1= "Dr. Mukesh Kumar Nagar," position1="Assistant Professor"></Box4b>
                 <Box4b name1= "Dr. Shashank Goel," position1="Assistant Professor"></Box4b>
             </div>
-            <div style={{marginLeft:"30px"}}>
-                <Box4b name1= "Prof. Lokendra Kumar," position1="Professor"></Box4b>
-                <Box4b name1= "Dr.  Lakhveer Kaur," position1="Associate Professor"></Box4b>
+            <div className='box-4style'>
                 <Box4b name1= "Dr. Neha Ahlawat," position1="Assistant Professor"></Box4b>
                 <Box4b name1= "Dr. Mohd. Sarfaraz," position1="Assistant Professor"></Box4b>
                 <Box4b name1= "Dr. Shikha Pandey," position1="Assistant Professor"></Box4b>
--- src/pages/Organizers.jsx
+++ src/pages/Organizers.jsx
@@ -15,11 +15,33 @@ function Organizers() {
         <Box1 post = "PATRONS" name1= "Prof. S. C. Saxena," position1 = "Pro Chancellor, JIIT" name2 = "Prof. B. R. Mehta," position2 = "Vice Chancellor, JIIT"/>
       </div>
       <br /><br />
-      <Box2 post = "CONFERENCE CHAIR" name1= "Prof. Alka Tripathi," position1 = "Pro Chancellor, JIIT"/>
+      <Box2 post = "CONFERENCE CHAIR" name1= "Prof. Alka Tripathi," position1 = "Head, Department of Mathematics"/>
       <Box2 post = "CONVENERS" name1= "Dr. Pankaj K. Srivastava & Dr. Dinesh C. S. Bisht" position1 = "Associate Professor(s)"/>
       <br />
       <br />
-      <div className='style'>
+      <div className="style2">
+        <div className='style'>
+          <Box3 post = "TRACK MANAGEMENT CHAIR" name1= "Dr. Pato Kumari" position1 = "Associate Professor"/>
+          <div className='style-special'></div>
+          <Box3 post = "PUBLICATION CHAIR" name1= "Dr. Yogesh Gupta" position1 = "Associate Professor"/>
+          
+        </div>
+        <div className='style'>
+          <div className='style-special'></div>
+          <Box3 post = "FINANCE CHAIR" name1= "Dr. Anuj Bhardwaj" position1 = "Associate Professor"/>
+          <div className='style-special2'></div>
+          <Box3 post = "HOSPITALITY CHAIR" name1= "Dr.  Himanshu Agarwal" position1 = "Assistant Professor"/>
+          
+          
+          
+        </div>
+        <div className='style'>
+          <Box3 post = "PUBLICITY & BROADCAST CHAIR" name1= "Dr. Amita Bhagat" position1 = "Assistant Professor"/>
+          <div className='style-special'></div>
+          <Box3 post = "REGISTRATION CHAIR" name1= "Dr. Richa Sharma" position1 = "Assistant Professor" className= "style-special"/>
+        </div>
+      </div>
+      {/* <div className='style'>
         <div style={{marginRight:"50px"}}>
           <Box3 post = "TRACK MANAGEMENT CHAIR" name1= "Dr. Pato Kumari" position1 = "Associate Professor"/>
           <Box3 post = "FINANCE CHAIR" name1= "Dr. Anuj Bhardwaj" position1 = "Associate Professor"/>
@@ -30,7 +52,7 @@ function Organizers() {
           <Box3 post = "HOSPITALITY CHAIR" name1= "Dr.  Himanshu Agarwal" position1 = "Associate Professor"/>
           <Box3 post = "REGISTRATION CHAIR" name1= "Dr. Richa Sharma" position1 = "Associate Professor"/>
         </div>
-      </div>
+      </div> */}
       <br />
       <br />
       <br />
